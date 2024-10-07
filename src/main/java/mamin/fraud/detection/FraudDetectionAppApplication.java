c826790207d89780f9b5900d46ab22dc15034c7b
Amin <abdimamin@gmail.com>
2024-10-07 05:56:12 +0100

    initial setup

---
+++ src/main/java/mamin/fraud/detection/FraudDetectionAppApplication.java
@@ -0,0 +1,13 @@
+package mamin.fraud.detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetectionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudDetectionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mamin/fraud/detection/FraudDetectionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mamin.fraud.detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
